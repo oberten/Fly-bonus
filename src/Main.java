e1b0032d4e437caa6a30ef93925054ccb8be2170
unknown <oberten@mail.ru>
2022-12-13 15:00:10 +0300

    changed 1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 5000;
+        int bonus = 20;
+        int milli = price / bonus;
+
+
+        System.out.println(milli);
+
+
+    }
+}
+
